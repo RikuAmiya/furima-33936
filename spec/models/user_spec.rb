d64225323de1340593942ccf3ada4bf41bf503fc
RikuAmiya <77384318+RikuAmiya@users.noreply.github.com>
2021-02-16 18:54:19 +0900

    テストコードの実装、修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,13 +13,13 @@ RSpec.describe User, type: :model do
     it 'nicknameが空では登録できないこと' do
       @user.nickname = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Nicknameを入力してください")
+      expect(@user.errors.full_messages).to include("Nicknameが入力されていません。")
     end
 
     it 'emailが空では登録できないこと' do
       @user.email = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Emailを入力してください")
+      expect(@user.errors.full_messages).to include("Emailが入力されていません。")
     end
 
     it '重複したemailが存在する場合登録できないこと' do
@@ -27,56 +27,56 @@ RSpec.describe User, type: :model do
       another_user = FactoryBot.build(:user)
       another_user.email = @user.email
       another_user.valid?
-      expect(another_user.errors.full_messages).to include("Emailはすでに存在します")
+      expect(another_user.errors.full_messages).to include("Emailは既に使用されています。")
     end
 
     it 'family_nameが空では登録できないこと' do
       @user.family_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Family nameを入力してください")
+      expect(@user.errors.full_messages).to include("Family nameが入力されていません。")
     end
 
     it 'first_nameが空では登録できないこと' do
       @user.first_name = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("First nameを入力してください")
+      expect(@user.errors.full_messages).to include("First nameが入力されていません。")
     end
 
     it 'family_name_kanaが空では登録できないこと' do
       @user.family_name_kana = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Family name kanaを入力してください")
+      expect(@user.errors.full_messages).to include("Family name kanaが入力されていません。")
     end
 
     it 'first_name_kanaが空では登録できないこと' do
       @user.first_name_kana = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("First name kanaを入力してください")
+      expect(@user.errors.full_messages).to include("First name kanaが入力されていません。")
     end
 
     it 'birthdayが空では登録できないこと' do
       @user.birthday = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Birthdayを入力してください")
+      expect(@user.errors.full_messages).to include("Birthdayが入力されていません。")
     end
 
     it 'passwordが空では登録できないこと' do
       @user.password = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Passwordを入力してください")
+      expect(@user.errors.full_messages).to include("Passwordが入力されていません。")
     end
 
-    it 'passwordが6文字以下であれば登録できないこと' do
-      @user.password = '000000'
-      @user.password_confirmation = '000000'
+    it 'passwordが5文字以下であれば登録できないこと' do
+      @user.password = '00000'
+      @user.password_confirmation = '00000'
       @user.valid?
-      expect(@user.errors.full_messages).to include()
+      expect(@user.errors.full_messages).to include("Passwordは6文字以上に設定して下さい。")
     end
 
     it 'passwordが存在してもpassword_confirmationが空では登録できないこと' do
       @user.password_confirmation = ''
       @user.valid?
-      expect(@user.errors.full_messages).to include("Password confirmationとPasswordの入力が一致しません")
+      expect(@user.errors.full_messages).to include("Password confirmationが内容とあっていません。")
     end
   end
 end
